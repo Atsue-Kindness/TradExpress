ac7950fec00382d4d600b6afcc98d4c141daa77d
Kindness-Atsue <83228996+AtsueKindness@users.noreply.github.com>
2022-05-01 21:56:02 +0100

    first commit

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -5,74 +5,76 @@ import "../App.css";
 
 const Data = () => {
   return (
-    <div className="overall-container">
-      <div className="data-container">
-        <div className="box-1">
-          <div className="inner-box">
-            <div className="mini-box">
-              <h3>BTC/NGN</h3>
-              <li>
-                <span>-3.21%</span>
-                <FaLongArrowAltDown className="down-arrow" />
-              </li>
-            </div>
-            <p>20,560.101.59 NGN</p>
-          </div>
-          <div className="outer-line"></div>
-        </div>
-
-        <div className="box-1">
-          <div className="inner-box">
-            <div className="mini-box">
-              <h3>USDT/NGN</h3>
-              <li>
-                <span>+10.20%</span>
-                <FaLongArrowAltUp className="arrow-up" />
-              </li>
-            </div>
-
-            <p>20,560.101.59 NGN</p>
-          </div>
-          <div className="outer-line"></div>
-        </div>
-
-        <div className="box-1">
-          <div className="inner-box">
-            <div className="mini-box">
-              <h3>ETH/NGN</h3>
-              <li>
-                <span>+253</span>
-                <FaLongArrowAltUp className="arrow-up" />
-              </li>
-            </div>
-
-            <p>818,490.32 NGN</p>
-          </div>
-          <div className="outer-line"></div>
-        </div>
-
-        <div className="box-1">
-          <div className="inner-box">
-            <div className="mini-box">
-              <h3>LTC/NGN</h3>
-              <li>
-                <span>-254</span>
-                <FaLongArrowAltDown className="down-arrow" />
-              </li>
-            </div>
-
-            <p>81,002.32 NGN</p>
-          </div>
-          <div className="outer-line"></div>
-        </div>
-
-        <div className="box-1">
+    <div className="datammarg">
+      <div className="overall-container">
+        <div className="data-container">
           <div className="box-1">
-            <h3>XRP/N</h3>
-
-            <p>226.5€</p>
+            <div className="inner-box">
+              <div className="mini-box">
+                <h3>BTC/NGN</h3>
+                <li>
+                  <span>-3.21%</span>
+                  <FaLongArrowAltDown className="down-arrow" />
+                </li>
+              </div>
+              <p>20,560.101.59 NGN</p>
+            </div>
+            <div className="outer-line"></div>
           </div>
-          <div className="outer-line"></div>
+
+          <div className="box-1">
+            <div className="inner-box">
+              <div className="mini-box">
+                <h3>USDT/NGN</h3>
+                <li>
+                  <span>+10.20%</span>
+                  <FaLongArrowAltUp className="arrow-up" />
+                </li>
+              </div>
+
+              <p>20,560.101.59 NGN</p>
+            </div>
+            <div className="outer-line"></div>
+          </div>
+
+          <div className="box-1">
+            <div className="inner-box">
+              <div className="mini-box">
+                <h3>ETH/NGN</h3>
+                <li>
+                  <span>+253</span>
+                  <FaLongArrowAltUp className="arrow-up" />
+                </li>
+              </div>
+
+              <p>818,490.32 NGN</p>
+            </div>
+            <div className="outer-line"></div>
+          </div>
+
+          <div className="box-1">
+            <div className="inner-box">
+              <div className="mini-box">
+                <h3>LTC/NGN</h3>
+                <li>
+                  <span>-254</span>
+                  <FaLongArrowAltDown className="down-arrow" />
+                </li>
+              </div>
+
+              <p>81,002.32 NGN</p>
+            </div>
+            <div className="outer-line"></div>
+          </div>
+
+          <div className="box-1">
+            <div className="box-1">
+              <h3>XRP/N</h3>
+
+              <p>226.5€</p>
+            </div>
+            <div className="outer-line"></div>
+          </div>
         </div>
       </div>
     </div>
--- src/Components/Testimonial.js
+++ src/Components/Testimonial.js
@@ -6,62 +6,64 @@ import arrow from "../Images/arrow.svg";
 
 const Testimonial = () => {
   return (
-    <div className="testimonial-sect">
-      <h2>Customers Review</h2>
-      <div className="testimonial-container">
-        <div className="test-box-one">
-          <div className="quotess">
-            <img src={quote} alt="" />
-            <p>
-              Trading on TradExpress mobile & web platforms has been a smooth
-              experience for me quite easy to navigate
-            </p>
-          </div>
-          <img width={50} src={arrow} alt="" className="quote-arrow" />
+    <div className="test-mag">
+      <div className="testimonial-sect">
+        <h2>Customers Review</h2>
+        <div className="testimonial-container">
+          <div className="test-box-one">
+            <div className="quotess">
+              <img src={quote} alt="" />
+              <p>
+                Trading on TradExpress mobile & web platforms has been a smooth
+                experience for me quite easy to navigate
+              </p>
+            </div>
+            <img width={50} src={arrow} alt="" className="quote-arrow" />
 
-          <div className="person">
-            <img width={80} src={person} alt="" className="persons" />
-            <div>
-              <h5>Ireti</h5>
-              <p>Nigeria</p>
+            <div className="person">
+              <img width={80} src={person} alt="" className="persons" />
+              <div>
+                <h5>Ireti</h5>
+                <p>Nigeria</p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="test-box-one">
-          <div className="quotess">
-            <img src={quote} alt="" />
-            <p>
-              They have the best rate compared to other platforms with fast
-              payout.
-            </p>
-          </div>
-          <img width={50} src={arrow} alt="" className="quote-arrow" />
+          <div className="test-box-one">
+            <div className="quotess">
+              <img src={quote} alt="" />
+              <p>
+                They have the best rate compared to other platforms with fast
+                payout.
+              </p>
+            </div>
+            <img width={50} src={arrow} alt="" className="quote-arrow" />
 
-          <div className="person">
-            <img width={80} src={person} alt="" className="persons" />
-            <div>
-              <h5>Bayo</h5>
-              <p>USA</p>
+            <div className="person">
+              <img width={80} src={person} alt="" className="persons" />
+              <div>
+                <h5>Bayo</h5>
+                <p>USA</p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="test-box-one">
-          <div className="quotess">
-            <img src={quote} alt="" />
-            <p>
-              Easy to fund and withdraw coins or cash on their platform, I will
-              definitely trade with them again.
-            </p>
-          </div>
-          <img width={50} src={arrow} alt="" className="quote-arrow" />
+          <div className="test-box-one">
+            <div className="quotess">
+              <img src={quote} alt="" />
+              <p>
+                Easy to fund and withdraw coins or cash on their platform, I
+                will definitely trade with them again.
+              </p>
+            </div>
+            <img width={50} src={arrow} alt="" className="quote-arrow" />
 
-          <div className="person">
-            <img width={80} src={person} alt="" className="persons" />
-            <div>
-              <h5>Olagoke</h5>
-              <p>Nigeria</p>
+            <div className="person">
+              <img width={80} src={person} alt="" className="persons" />
+              <div>
+                <h5>Olagoke</h5>
+                <p>Nigeria</p>
+              </div>
             </div>
           </div>
         </div>
